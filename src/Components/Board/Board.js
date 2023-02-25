5a0631d0ac904e6b7328733ffb6a6225e9b66de5
chyga2 <chyga2@yandex.ru>
2023-02-25 23:13:33 +0300

    check 6

--- src/Components/Board/Board.js
+++ src/Components/Board/Board.js
@@ -7,41 +7,42 @@ import {Link} from "react-router-dom";
 
 function Board(props) {
     return (
-        <div className="board">
-            
-            <div className="board_cards custom-scroll">
-            <div className="board_header">
-                <p className="board_header_title ">
-                    {props.board?.title}
-                </p>
-            </div>
-                {props.board?.cards?.map((item) => (
-                    <Link className="a" to={`/${item.id}`} key={item.id} state={{title: item.title}}>
-                        <Card
-                            key={item.id}
-                            card={item}
-                            boardId={props.board.id
-                            }
+        <div className="b">
+            <div className="board">            
+                <div className="board_cards custom-scroll">
+                    <div className="board_header">
+                        <p className="board_header_title ">
+                            {props.board?.title}
+                        </p>
+                    </div>
+                    {props.board?.cards?.map((item) => (
+                        <Link className="a" to={`/${item.id}`} key={item.id} state={{title: item.title}}>
+                            <Card
+                                key={item.id}
+                                card={item}
+                                boardId={props.board.id
+                                }
+                            />
+                        </Link>
+                    ))}
+                    {props.board.id === 1 ?
+                        <AddCard
+                            text="+ Add Card"
+                            placeholder="Enter Card Title"
+                            displayClass="board_add-card"
+                            editClass="board_add-card_edit"
+                            onSubmit={(value) => props.addCard(props.board?.id, value)}
                         />
-                    </Link>
-                ))}
-                {props.board.id === 1 ?
-                    <AddCard
-                        text="+ Add Card"
-                        placeholder="Enter Card Title"
-                        displayClass="board_add-card"
-                        editClass="board_add-card_edit"
-                        onSubmit={(value) => props.addCard(props.board?.id, value)}
-                    />
-                    : (
-                        <DropdownButton
-                            buttonText="+ Add card"
-                            options={props.prevBoardCards.map(el => el.title)}
-                            onSelect={(props.moveCard)}
-                            id={(props.id)}
-                        />
-                    )}
-            </div>
+                        : (
+                            <DropdownButton
+                                buttonText="+ Add card"
+                                options={props.prevBoardCards.map(el => el.title)}
+                                onSelect={(props.moveCard)}
+                                id={(props.id)}
+                            />
+                        )}
+                </div>
+            </div>    
         </div>
     );
 }
