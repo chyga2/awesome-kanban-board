b083662ea08eb976b429dab23f90cb30d121e954
chyga2 <chyga2@yandex.ru>
2023-02-26 05:35:39 +0300

    commit before msc

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,13 +1,13 @@
 import React from "react";
-
+import './Footer.css'
 function Footer(props) {
 
     return (
         <div>
             <div className="app_footer">
-                <p>Active tasks: {props.active}</p>
-                <p>Finished tasks: {props.finished}</p>
-                <p>Kanban Board by Darina, 2023</p>
+                <p className="marg_curent">Active tasks: {props.active}</p>
+                <p className="marg_curent">Finished tasks: {props.finished}</p>
+                <p className="marg_by">Kanban Board by Darina, 2023</p>
             </div>
         </div>
     );
